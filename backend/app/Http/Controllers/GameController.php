7f78b9401a720ad07d1713988836a62d8ea61d2b
Nikola <djura328@yahoo.com>
2020-03-08 09:48:18 +0100

    GameController

---
+++ backend/app/Http/Controllers/GameController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Game;
+use Illuminate\Http\Request;
+
+class GameController extends Controller
+{
+
+    public function index(){
+        return Game::all();
+    }
+
+    public function createGame(Request $request){
+
+        $game = new Game;
+
+        $game->name = $request->name;
+
+        $game->save();
+
+        if($game) return response()->json($game, 200);
+        return response()->json(['status' => 'failed'], 500);
+
+    }
+}
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -14,6 +14,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::post('/games', 'GameController@createGame'); // "name": string
+Route::get('/games', 'GameController@index');
