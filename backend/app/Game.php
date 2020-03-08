4f24339e6154b94ef68e8d544c4a1ef1e25d1085
Nikola <djura328@yahoo.com>
2020-03-08 10:54:59 +0100

    game model add methods

--- backend/app/Game.php
+++ backend/app/Game.php
@@ -6,8 +6,31 @@ use Illuminate\Database\Eloquent\Model;
 
 class Game extends Model
 {
-    public function armies()
-    {
+    public function armies(){
+
         return $this->hasMany('App\Army');
+    }
+
+    public function restart(){
+
+        $this->armies()->update(['status' => 1]);
+        $this->armies()->each(function($amies){
+            $amies->units()->update(['status' => 1, 'health' => 1]);
+        });
+    }
+
+    public static function restartAll(){
+
+        $games = self::all();
+
+        collect($games)->each(function($game){
+
+            $game->armies()->update(['status' => 1]);
+
+            $game->armies()->each(function($amies){
+                $amies->units()->update(['status' => 1, 'health' => 1]);
+            });
+        });
+
     }
 }
